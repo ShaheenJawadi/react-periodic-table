312d42689fbf0106788d1ff70ca8196657cf4851
shaheenj <jawadishaheen@gmail.com>
2024-01-06 17:08:20 +0100

    add @google/model-viewer : visualize element

--- src/app/components/drawer.tsx
+++ src/app/components/drawer.tsx
@@ -1,10 +1,25 @@
 
+import   '@google/model-viewer';
+import  { ModelViewerElement } from '@google/model-viewer';
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'model-viewer': Partial<ModelViewerElement>;
+        }
+    }
+}
+
 
 const BottomDrawer=()=>{
     return (
         
         <div  className="DrawerContainer">
-            
+            <model-viewer alt="qdqsd" src="https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen.glb" ar    
+            shadow-intensity="1" camera-controls touch-action="pan-y">
+
+            </model-viewer>
+            {/* https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen.glb */}
         </div>
     )
 
