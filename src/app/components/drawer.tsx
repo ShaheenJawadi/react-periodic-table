6ea22b11f9d1e8b7cae96c1b77bedb9ca5b8674e
shaheenj <jawadishaheen@gmail.com>
2024-01-21 21:06:24 +0100

    restyle drawer:  close button

--- src/app/components/drawer.tsx
+++ src/app/components/drawer.tsx
@@ -17,23 +17,24 @@ const BottomDrawer=()=>{
     return (
         
         <div  className="drawerContainer">
-            <div className='drawerHeader' >
-                <div className='name' >
-                    {drawerData?.name}
-                </div>
+        
+               
                 <div onClick={() => closeDrawer()} className='close'>X</div>
-
-            </div>
+ 
          
             <div className='content' >
                     <div className='model3d' >
-                    <model-viewer min-field-of-view="1deg" alt="qdqsd" src={drawerData?.bohr_model_3d} 
-                        shadow-intensity="0" rotation-per-second="60deg"  camera-controls  >
+                        <model-viewer min-field-of-view="1deg" alt="qdqsd" src={drawerData?.bohr_model_3d} 
+                            shadow-intensity="0" rotation-per-second="60deg"  camera-controls  >
                         </model-viewer>
 
                     </div>
                     <div className='elementData' >
-                        data
+                    <div className='name' >
+                        {drawerData?.name}
+                    </div>
+                    {drawerData?.summary}
+              
                     </div>
                     <div className='elementImage' >
                         <img src={drawerData?.image.url} alt={drawerData?.image?.attribution} />
